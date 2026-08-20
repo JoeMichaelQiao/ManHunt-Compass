6e4f15dbf249578eea6dac446cf9d5909ee57f68
Joyce Qiao <61264632+JoeMichaelQiao@users.noreply.github.com>
2026-08-20 15:50:35 +0800

    Configure plugin and dependency management in settings
    Added plugin and dependency resolution management for Gradle.

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,1 +1,17 @@
+pluginManagement {
+    repositories {
+        gradlePluginPortal()
+        mavenCentral()
+        maven { url = uri("https://maven.fabricmc.net/") } // 用于 fabric-loom 插件
+    }
+}
+
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        mavenCentral()
+        maven { url = uri("https://maven.fabricmc.net/") } // 用于 fabric 依赖
+    }
+}
+
 rootProject.name = "manhunt-compass"
